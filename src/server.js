89cdb93497308788bd88a59c465a5daafe334b4e
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-19 23:39:18 +0600

    feat: Added users/users.model.js | improve: Updated src/server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import app from "./app.js";
+
 import { connectToDb } from "./App/config/db.js";
 // /run/media/sj/developer/web/L1B11/11mi/ass/EduVerse/server-EduVerse/src/server.js
 const port = process.env.PORT || 5000;
