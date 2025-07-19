14d5bd6ae3208c3acac9494cbbc4b952b4118e04
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-20 00:00:18 +0600

    improve: Updated users/users.model.js

---
+++ src/App/modules/users/users.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    photoURL: {
+      type: String,
+      default: "",
+    },
+    role: {
+      type: String,
+      enum: ["tourist", "tour-guide", "admin"],
+      default: "tourist",
+    },
+    last_loggedIn: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true, // Automatically creates `createdAt` and `updatedAt`
+  },
+);
+
+export const User = mongoose.model("User", userSchema);
