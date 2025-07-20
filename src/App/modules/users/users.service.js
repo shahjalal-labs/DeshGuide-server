c70a27666a09166798c55c81dc1debb2eb0390d1
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-20 12:19:38 +0600

    feat: Added users/users.service.js

---
+++ src/App/modules/users/users.service.js
@@ -0,0 +1,37 @@
+// src/App/modules/users/users.service.js
+import User from "./users.model.js";
+
+// ✅ Get All Users
+const getAllUsersFromDB = async () => {
+  return await User.find().sort({ createdAt: -1 });
+};
+
+// ✅ Get Single User by ID
+const getUserByIdFromDB = async (id) => {
+  return await User.findById(id);
+};
+
+// ✅ Update User
+const updateUserInDB = async (id, updatedData) => {
+  return await User.findByIdAndUpdate(id, updatedData, {
+    new: true,
+    runValidators: true,
+  });
+};
+
+// ✅ Delete User
+const deleteUserFromDB = async (id) => {
+  const user = await User.findById(id);
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  return await User.findByIdAndDelete(id);
+};
+
+export const UserServices = {
+  getAllUsersFromDB,
+  getUserByIdFromDB,
+  updateUserInDB,
+  deleteUserFromDB,
+};
