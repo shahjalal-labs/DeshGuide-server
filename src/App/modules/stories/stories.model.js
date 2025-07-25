fec96f12d02199e953619c58e3d8f96674a25e81
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-25 16:14:44 +0600

    improve: Updated stories/stories.model.js, stories/storiesApi.hurl

--- src/App/modules/stories/stories.model.js
+++ src/App/modules/stories/stories.model.js
@@ -1,6 +1,45 @@
-// //
-// import { Schema, model } from "mongoose";
 //
+import { Schema, model } from "mongoose";
+
+const storySchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [String],
+      default: [],
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      immutable: true, // ❌ Cannot be changed after creation
+    },
+    userName: {
+      type: String,
+      required: true,
+      immutable: true, // ❌ Prevent mutation
+    },
+    userPhoto: {
+      type: String,
+      required: true,
+      immutable: true, // ❌ Prevent mutation
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Story = model("Story", storySchema);
+
+// import { Schema, model } from "mongoose";
 // const storySchema = new Schema(
 //   {
 //     title: {
